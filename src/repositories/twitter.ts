ddeb927ba67fc4fa78fe8855c41b7cc2e9dc60f9
tsubasa <tsubasan1122@gmail.com>
2022-08-14 23:54:04 +0900

    dayjsをinstallした

--- src/repositories/twitter.ts
+++ src/repositories/twitter.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import Client from "twitter-api-sdk";
 import Tweet from "../models/tweet";
 
@@ -11,15 +12,19 @@ export default class TwitterRepository {
   async searchTweets(keyword: string): Promise<Tweet[]> {
     // 引用リツイート・リツイート・リプライのツイートを取り除く
     const query = `${keyword} -is:quote -is:reply -is:retweet`;
+    // 一日分のツイートを取得する
+    const start_time = dayjs().subtract(1, "day").startOf("day").toISOString();
     const response = await this.client.tweets.tweetsRecentSearch({
       query: query,
       max_results: 100,
       expansions: ["author_id"],
       "tweet.fields": ["text"],
+      start_time: start_time,
     });
 
     if (!response.data) return [];
 
+    console.log(response);
     return response.data.map(
       (res) => new Tweet(res.id, res.author_id, res.text)
     );
